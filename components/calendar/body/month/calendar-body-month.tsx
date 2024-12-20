3de32055537e02ae2c172652667bd82a56339981
Charlie Lamb <charlielamb20@icloud.com>
2024-12-20 18:45:07 +0000

    ui change

--- components/calendar/body/month/calendar-body-month.tsx
+++ components/calendar/body/month/calendar-body-month.tsx
@@ -69,7 +69,7 @@ export default function CalendarBodyMonth() {
               <div
                 className={cn(
                   'text-sm font-medium w-fit p-1 flex flex-col items-center justify-center rounded-full aspect-square',
-                  isToday && 'bg-primary text-primary-foreground'
+                  isToday && 'bg-primary text-background'
                 )}
               >
                 {format(day, 'd')}
